466752f0f876434f27352cf787d483c1049b33fa
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-08-23 22:37:26 -0300

    Modifico gitignore y le doy formato al proyecto

---
+++ Source/Taller.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Taller.cpp
+// Author      : Jorge
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Primer main" << endl; // prints tp---taller
+	return 0;
+}
