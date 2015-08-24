48171e001db72614c8d16ecc30117c20156964b0
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-08-23 23:34:49 -0300

    dejo comentado unas lineas para probar la libreria sdl, pero hay problemas de dependencias

--- Source/Taller.cpp
+++ Source/Taller.cpp
@@ -7,9 +7,15 @@
 //============================================================================
 
 #include <iostream>
+//#include "SDL/SDL.h"
+
 using namespace std;
 
 int main() {
+	//Start SDL
+	//SDL_Init( SDL_INIT_EVERYTHING );
+	//Quit SDL
+	//SDL_Quit();
 	cout << "Primer main" << endl; // prints tp---taller
 	return 0;
 }
