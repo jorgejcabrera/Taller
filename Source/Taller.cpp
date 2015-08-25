dabffa6a99076de76e384a5b38fda66edce86d48
jorgejcabrera <jorgejcabrera@hotmail.com.ar>
2015-08-25 18:08:06 -0300

    probando la libreria

--- Source/Taller.cpp
+++ Source/Taller.cpp
@@ -11,11 +11,39 @@
 
 using namespace std;
 
+SDL_Surface *demo_screen;
+
 int main( int argc, char* args[] ) {
 	//Start SDL
-	SDL_Init( SDL_INIT_EVERYTHING );
-	//Quit SDL
+//	if (SDL_Init( SDL_INIT_EVERYTHING )!=0)
+//		fprintf(stderr,"Could not initialize SDL: %s\n",SDL_GetError());
+//	printf("SDL Initialized\n");
+//
+//
+//	//Quit SDL
+//	SDL_Quit();
+//	printf("SDL Shutdown\n");
+//	return 0;
+
+	SDL_Event ev;
+	int active;
+	/* Initialize SDL */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+	/* Open main window */
+	demo_screen = SDL_SetVideoMode(320, 240, 0, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	if (!demo_screen)
+		fprintf(stderr, "Could not set video mode: %s\n", SDL_GetError());
+	/* Main loop */
+	active = 1;
+	while (active) {
+		/* Handle events */
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT)
+				active = 0; /* End */
+		}
+	}
+	/* Exit */
 	SDL_Quit();
-	cout << "Primer main" << endl; // prints tp---taller
 	return 0;
 }
