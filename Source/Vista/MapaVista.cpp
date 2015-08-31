812f2570ddca7e50c0cffcc6b213223f365b6eb7
jorgejcabrera <cabrerajjorge@gmail.com>
2015-08-31 20:32:20 -0300

    muevo la logica de la vista

---
+++ Source/Vista/MapaVista.cpp
@@ -0,0 +1,50 @@
+/*
+ * MapaVista.cpp
+ *
+ *  Created on: 31 de ago. de 2015
+ *      Author: jorge
+ */
+
+#include "MapaVista.h"
+
+namespace std {
+
+MapaVista::MapaVista() {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+
+}
+
+MapaVista::~MapaVista() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Source/Vista/MapaVista.h
@@ -0,0 +1,23 @@
+/*
+ * MapaVista.h
+ *
+ *  Created on: 31 de ago. de 2015
+ *      Author: jorge
+ */
+
+#ifndef SOURCE_VISTA_MAPAVISTA_H_
+#define SOURCE_VISTA_MAPAVISTA_H_
+
+#include "SDL2/SDL.h"
+
+namespace std {
+
+class MapaVista {
+public:
+	MapaVista();
+	virtual ~MapaVista();
+};
+
+} /* namespace std */
+
+#endif /* SOURCE_VISTA_MAPAVISTA_H_ */
