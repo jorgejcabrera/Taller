9698be4a36229dd75466e4d018c3bf877e8a909b
Jorge Cabrera <jocabrera@ARAR-jocabrera.local>
2015-09-08 10:30:11 -0300

    refactor JuegoVista::drawIsometricView: obtengo un puntero al Tile y no una copia del tile

--- Source/Vista/JuegoVista.cpp
+++ Source/Vista/JuegoVista.cpp
@@ -11,10 +11,10 @@ void JuegoVista::drawIsometricMap(SDL_Texture *image){
 	int posX = 0;
 	int posY = 0;
 	for (map<pair<int,int>,Tile>::iterator it = this->mapa->getTiles()->begin(); it != this->mapa->getTiles()->end();++it){
-		Tile tileActual = (*it).second;
+		Tile* tileActual = &((*it).second);
 		//transformo coordenadas cartesianas a isométricas
-		posY = (tileActual.getPosX()+tileActual.getPosY()) * DefaultSettings::getTileSize() / 2;
-		posX = (tileActual.getPosX()-tileActual.getPosY()) * DefaultSettings::getTileSize() + DefaultSettings::getScreenWidth() / 2;	//comienzo a dibujar de la mitad de la pantalla
+		posY = (tileActual->getPosX()+tileActual->getPosY()) * DefaultSettings::getTileSize() / 2;
+		posX = (tileActual->getPosX()-tileActual->getPosY()) * DefaultSettings::getTileSize() + DefaultSettings::getScreenWidth() / 2;	//comienzo a dibujar de la mitad de la pantalla
 		picassoHelper->renderTexture(image,posX,posY,  DefaultSettings::getTileSize() * 2, DefaultSettings::getTileSize());
 	}
 }
