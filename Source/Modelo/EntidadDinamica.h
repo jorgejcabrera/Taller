39a32ad280649d0e1819d7dd6594a6fba02f2e74
JuanGonzalez9 <juanl9sd@gmail.com>
2015-09-11 11:12:51 -0300

    subo el modelo de entidad dinamica

--- Source/Modelo/EntidadDinamica.h
+++ Source/Modelo/EntidadDinamica.h
@@ -14,10 +14,32 @@ using namespace std;
 
 class EntidadDinamica: public EntidadPartida {
 	private:
+
 		int velocidad;
+		float velX;
+		float velY;
+
+		bool caminando;
+
+		float posX;
+		float posY;
+		float destinoX;
+		float destinoY;
+
+		float distanciaA(float x, float y);
+
+		float distanciaEnX(float x);
+		float distanciaEnY(float y);
+
 	public:
-		EntidadDinamica();
+		EntidadDinamica(int vel,float posX,float posY);
 		virtual ~EntidadDinamica();
+
+		// creo que no hace falta que sean publicos estos gets
+		float getX();
+		float getY();
+
+		void setDestino(float x, float y);
 		void trasladarse();
 };
 
