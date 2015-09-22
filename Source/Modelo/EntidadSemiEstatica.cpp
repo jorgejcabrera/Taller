da26478aa75509a77f6f5fb289a404c7aec4beab
jorgejcabrera <cabrerajjorge@gmail.com>
2015-09-22 00:02:42 -0300

    creadas la clase semi estatica

---
+++ Source/Modelo/EntidadSemiEstatica.cpp
@@ -0,0 +1,21 @@
+/*
+ * EntidadSemiEstatica.cpp
+ *
+ *  Created on: 22 de set. de 2015
+ *      Author: jorge
+ */
+
+#include "EntidadSemiEstatica.h"
+
+namespace std {
+
+EntidadSemiEstatica::EntidadSemiEstatica() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EntidadSemiEstatica::~EntidadSemiEstatica() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Source/Modelo/EntidadSemiEstatica.h
@@ -0,0 +1,23 @@
+/*
+ * EntidadSemiEstatica.h
+ *
+ *  Created on: 22 de set. de 2015
+ *      Author: jorge
+ */
+
+#ifndef SOURCE_MODELO_ENTIDADSEMIESTATICA_H_
+#define SOURCE_MODELO_ENTIDADSEMIESTATICA_H_
+
+#include "EntidadPartida.h"
+
+namespace std {
+
+class EntidadSemiEstatica: public EntidadPartida {
+public:
+	EntidadSemiEstatica();
+	virtual ~EntidadSemiEstatica();
+};
+
+} /* namespace std */
+
+#endif /* SOURCE_MODELO_ENTIDADSEMIESTATICA_H_ */
