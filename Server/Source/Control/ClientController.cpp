efb17b10935a06e1d03946e5933091d1718dc129
jorgejcabrera <cabrerajjorge@gmail.com>
2015-10-03 17:43:27 -0300

    header handler para los clientes

---
+++ Server/Source/Control/ClientController.cpp
@@ -0,0 +1,18 @@
+/*
+ * ClientController.cpp
+ *
+ *  Created on: 3 de oct. de 2015
+ *      Author: jorge
+ */
+
+#include "ClientController.h"
+
+ClientController::ClientController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ClientController::~ClientController() {
+	// TODO Auto-generated destructor stub
+}
+
